f2af3d4e41fece93185ecf115cbd2d4b15d87c02
c17julka <c17julka@student.his.se>
2019-03-19 02:36:10 +0100

    Added cutie clicker

--- SpeedClicker/src/Main.java
+++ SpeedClicker/src/Main.java
@@ -1,4 +1,13 @@
+import java.io.IOException;
 
-public class Main {
+import javax.swing.JFrame;
+
+public class Main 
+{
+
+	public static void main(String[] args)
+	{
+		JFrame frame = new Frame("Speedclicker");
+	}
 
 }
