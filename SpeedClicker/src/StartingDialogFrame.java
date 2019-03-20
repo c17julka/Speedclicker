6bbb79460d9c79d5d4470e8e78eed2f88ed6fda3
c17julka <c17julka@student.his.se>
2019-03-20 02:23:48 +0100

    Starting dialog edits

--- SpeedClicker/src/StartingDialogFrame.java
+++ SpeedClicker/src/StartingDialogFrame.java
@@ -1,3 +1,4 @@
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,7 +14,7 @@ public class StartingDialogFrame extends JFrame
 	{		
 		// Frame settings
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(200, 160);
+		setSize(320, 200);
 		setLayout(new GridBagLayout());
 		setLocationRelativeTo(null);
 		setVisible(true);
@@ -33,7 +34,41 @@ public class StartingDialogFrame extends JFrame
 			
 		});
 		
-		add(startBtn);
+		JLabel welcome = new JLabel("Welcome to Speedclicker!");
+		JLabel rule1 = new JLabel("Click the creature to gain points");
+		JLabel rule2 = new JLabel("Shop for items to auto increase your points");
+		JLabel rule3 = new JLabel("You have 1 minute");
+		
+		// Add components to frame
+		GridBagConstraints gbc = new GridBagConstraints();
+	    
+	    gbc.weightx = 1;
+		gbc.weighty = 1;
+		
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		
+		add(welcome, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		
+		add(rule1, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		
+		add(rule2, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		
+		add(rule3, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+		
+		add(startBtn, gbc);
 		
 	}
 	
